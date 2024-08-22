e2a4eaeedada29c67aa6afb4575b3a8abf8e8eab
ChikaNwosu <165509397+ChikaNwosu@users.noreply.github.com>
2024-08-21 21:49:50 -0700

    Updated edits

---
+++ aboutme.js
@@ -0,0 +1,4 @@
+let firstName = "Chikaodili";
+let lastName = "Nwosu";
+
+document.getElementById("fullName").textContent = firstName + " " + lastName;
---
+++ script.js
@@ -0,0 +1,5 @@
+function onclickButton() {
+    let programmingLag = prompt("What is your favourite programming language?");
+
+    document.getElementById("faveProg").textContent = programmingLag;
+}
